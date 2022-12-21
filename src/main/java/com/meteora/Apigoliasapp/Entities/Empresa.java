358eec5a44cbd54b7e8f126e959b77694ff26689
gabreudev <biel0alves0@gmail.com>
2022-12-21 09:09:05 -0200

    FEAT: initial commit

--- src/main/java/com/meteora/Apigoliasapp/Entities/Empresa.java
+++ src/main/java/com/meteora/Apigoliasapp/Entities/Empresa.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Getter
 @Setter
-@Data
+//@Data
 @Table
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/meteora/Apigoliasapp/Entities/Estagiario.java
+++ src/main/java/com/meteora/Apigoliasapp/Entities/Estagiario.java
@@ -10,7 +10,7 @@ import lombok.*;
 
 @Getter
 @Setter
-@Data
+//@Data
 @Table
 public class Estagiario {
     @Id
